780455239f61895f004e69d6f5c9ad8e33666354
Recep Demir <demir.rp@gmail.com>
2025-10-21 14:26:01 +0200

    Refactor search button functionality to handle empty input and improve user feedback

--- script.js
+++ script.js
@@ -2,11 +2,71 @@ const searchBtn = document.getElementById('searchBtn');
 const wordInput = document.getElementById('wordInput');
 const result = document.getElementById('result');
 
-searchBtn.addEventListener("click", () => {
-    const word = wordInput.value
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const word = wordInput.value.trim()
+
+    if(word === ''){
+        result.innerHTML=`
+        <p>Please type a word</p>
+        `
+        return
+    }
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then(response => response.json())
+    .then(data => {
+        const myWord = data[0];
+        const phonetic = data[0].phonetics[1]
+        console.log(data)
+
+        result.innerHTML = `
+        <div class="card">
+        <h2>${data[0].word}</h2>
+        <div>${phonetic.text}</div>
+        <audio controls src=${phonetic.audio}></audio>
+
+        </div>
+
+
+        ${myWord.meanings.map(meaning => `
+        <div class="card">
+            <div class="muted">${meaning.partOfSpeech}</div>
+            <ol>
+                ${meaning.definitions.map((def, i) => `
+                <li>
+                ${def.definition}
+                ${def.example ? `<div class="muted">Example: “${def.example}”</div>` : ""}
+                </li> `).join("")}
+            </ol>
+
+                ${meaning.synonyms.length > 0 
+      ? `<div class="muted">Synonyms: ${meaning.synonyms.join(", ")}</div>` 
+      : ""}
+
+
+
+
+        </div>
+        `).join("")}
+
+        `
+    })
+    .catch(() => {
+      result.innerHTML = `<p>Something went wrong.</p>`;
+    });
 
 } )
 
-fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-.then(response => response.json)
-.then(data => console.log(data))
+
+wordInput.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    searchBtn.click();
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const word = wordInput.value.trim();
+  searchBtn.click();
+});
